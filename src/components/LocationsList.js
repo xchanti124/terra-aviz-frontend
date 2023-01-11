23382d62700e080d29b227a6a59ada4f9311b854
FlyingRatMan <olya.shalashova@gmail.com>
2023-01-11 22:08:04 +0100

    Added location and locationList components and corresponding sagas with slices

---
+++ src/components/LocationsList.js
@@ -0,0 +1,24 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+import Location from "./Location";
+
+const Locations = () => {
+  const { locations } = useSelector(state => state.locations);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: "LOCATIONS_REQUESTED" });
+  }, [dispatch]);
+
+  return (
+    <div>
+      {locations.map(location => (
+        <Location key={location._id} location={location} />
+      ))}
+    </div>
+  );
+};
+
+export default Locations;
