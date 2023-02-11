be9aedfa6640a1bc96ddc1c7349301288cf2e24e
meeragk99 <meeragk999@gmail.com>
2023-02-11 12:00:05 +0100

    implemented locationform

--- src/components/LocationForm.js
+++ src/components/LocationForm.js
@@ -3,27 +3,42 @@ import React, { useState } from "react";
 function LocationForm() {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [location, setLocation] = useState("");
-    const [pictures, setPictures] = useState([]);
+    const [address, setAddress] = useState("");
+    const [hashtags, setHashtags] = useState([]);
+    const [imageLink, setImageLink] = useState([]);
+    const [category, setCategory] = useState([]);
     const [errors, setErrors] = useState([]);
 
     const validate = () => {
         const formErrors = [];
-
         if (!title) formErrors.push("Title is required");
         if (!description) formErrors.push("Description is required");
-        if (!location) formErrors.push("Location is required");
-        if (!pictures.length) formErrors.push("At least one picture is required");
-
-        setErrors(formErrors);
+        if (!address) formErrors.push("Address is required");
+        if (!hashtags) formErrors.push("Hashtags are required");
+        if (!category) formErrors.push("Category is required");
+        if (!imageLink) formErrors.push("imageLink is required");
         return formErrors.length === 0;
     };
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
 
         if (validate()) {
-            console.log("Submitting form with:", { title, description, location, pictures });
+            console.log("Submitting form with:", { title, description, address,hashtags,category,imageLink});
+            let res = await fetch("http://localhost:3000/api/locations",{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams({
+                    title,
+                    description,
+                    address,
+                    imageLink,
+                    hashtags,
+                    category,
+                })
+            })
             // Additional functionality to handle form submission can be added here
         }
     };
@@ -53,26 +68,52 @@ function LocationForm() {
             {errors.includes("Description is required") && <p className="error">Description is required</p>}
 
             <label>
-                Location:
+                Address:
                 <input
                     type="text"
-                    value={location}
-                    onChange={({ target: { value } }) => setLocation(value)}
+                    value={address}
+                    onChange={({ target: { value } }) => setAddress(value)}
                     required
                 />
             </label>
-            {errors.includes("Location is required") && <p className="error">Location is required</p>}
+            {errors.includes("Address is required") && <p className="error">Address is required</p>}
+
+
 
             <label>
-                Pictures:
+                Hashtags:
                 <input
-                    type="file"
-                    multiple
-                    onChange={({ target: { files } }) => setPictures(Array.from(files))}
+                    type="text"
+                    value={hashtags}
+                    onChange={({ target: { value } }) => setHashtags(value)}
                     required
                 />
             </label>
-            {errors.includes("At least one picture is required") && <p className="error">At least one picture is required</p>}
+            {errors.includes("Hashtags are required") && <p className="error">Hashtags are required</p>}
+
+            <label>
+                imageLink:
+                <input
+                    type="text"
+                    value={imageLink}
+                    onChange={({target: { value} } ) => setImageLink(value)}
+                    required
+                />
+            </label>
+            {errors.includes("ImageLink is required") && <p className="error">ImageLink is required</p>}
+
+
+            <label>
+                category:
+                <input
+                    type="text"
+                    value={category}
+                    onChange={({target: { value} } ) => setCategory(value)}
+                    required
+                />
+            </label>
+            {errors.includes("Category is required") && <p className="error">Category is required</p>}
+
 
             <button type="submit">Submit</button>
         </form>
@@ -80,3 +121,4 @@ function LocationForm() {
 }
 
 export default LocationForm;
+
