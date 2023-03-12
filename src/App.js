e06b27b9ce46ed3d031a491444d0a5b3d4c9dea4
meeragk99 <meeragk999@gmail.com>
2023-03-12 23:41:41 +0100

    Add logout button

--- src/App.js
+++ src/App.js
@@ -7,28 +7,54 @@ import LocationForm from "./components/LocationForm";
 import Login from "./components/Login";
 
 import styles from "./styles/app.module.css";
+import { useEffect, useState } from "react";
+import { isAuthenticated, logout } from "./helpers";
 
-const App = () => (
-  <>
-    <nav className={styles.navigation}>
-      <NavLink to={"/"}> LOCATIONS </NavLink>
-      <NavLink to={"/login"}> LOGIN </NavLink>
-      <NavLink to={"/location_form"}> ADD LOCATION </NavLink>
-    </nav>
+const App = () => {
+  const [isLoggedIn, setLoggedIn] = useState(isAuthenticated());
 
-    <div className={styles.main}>
-      <Routes>
-        <Route path={"/"}>
-          <Route index element={<LocationsList />} />
-          <Route path={"details"} element={<Location />} />
-          <Route path={"location_form"} element={<LocationForm />} />
-          <Route path={"login"} element={<Login />} />
-        </Route>
-        {/* <Route path={"/new_location"} element={<LocationForm/>} /> */}
-        <Route path={"*"} element={<Error404 />} />
-      </Routes>
-    </div>
-  </>
-);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoggedIn(isAuthenticated());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <>
+      <nav className={styles.navigation}>
+        <NavLink to={"/"}> LOCATIONS </NavLink>
+        <NavLink to={"/location_form"}> ADD LOCATION </NavLink>
+        {!isLoggedIn && <NavLink to={"/login"}> LOGIN </NavLink>}
+        {isLoggedIn && (
+          <NavLink
+            to="#"
+            onClick={() => {
+              logout();
+            }}
+          >
+            LOGOUT
+          </NavLink>
+        )}
+      </nav>
+
+      <div className={styles.main}>
+        <Routes>
+          <Route path={"/"}>
+            <Route index element={<LocationsList />} />
+            <Route path={"details"} element={<Location />} />
+            <Route path={"location_form"} element={<LocationForm />} />
+            <Route path={"login"} element={<Login />} />
+          </Route>
+          {/* <Route path={"/new_location"} element={<LocationForm/>} /> */}
+          <Route path={"*"} element={<Error404 />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
 export default App;
