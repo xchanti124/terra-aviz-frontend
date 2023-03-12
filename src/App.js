d89f8fe6b9a02889a86210d4de7b0e044e8c5fc0
meeragk99 <meeragk999@gmail.com>
2023-03-12 22:50:12 +0100

    Add login form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import LocationsList from "./components/LocationsList";
 import Location from "./components/Location";
 import Error404 from "./components/Error404";
 import LocationForm from "./components/LocationForm";
+import Login from "./components/Login";
 
 import styles from "./styles/app.module.css";
 
 const App = () => (
   <>
     <nav className={styles.navigation}>
-      <NavLink to={"/new_location"}> CREATE NEW LOCATION </NavLink>
       <NavLink to={"/"}> LOCATIONS </NavLink>
       <NavLink to={"/login"}> LOGIN </NavLink>
       <NavLink to={"/location_form"}> ADD LOCATION </NavLink>
@@ -22,6 +22,7 @@ const App = () => (
           <Route index element={<LocationsList />} />
           <Route path={"details"} element={<Location />} />
           <Route path={"location_form"} element={<LocationForm />} />
+          <Route path={"login"} element={<Login />} />
         </Route>
         {/* <Route path={"/new_location"} element={<LocationForm/>} /> */}
         <Route path={"*"} element={<Error404 />} />
